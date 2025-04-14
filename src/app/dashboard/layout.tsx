d143388cd04d1fb517be19bb16f88389d80d0c45
Duverney muriel <duverwild@gmail.com>
2025-04-14 17:51:27 +0200

    intentanto desplegar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '@/app/dashboard/context/AuthContext';
 import MainLayout from '@/components/layout/MainLayout';
 
 export default function InicioLayout({
